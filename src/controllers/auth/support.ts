cccee633f9e3f83015362c1a905e9afe76126644
Jan Joshua <wawaguzman28@gmail.com>
2024-04-24 22:46:44 +0800

    Still doesn't

--- src/controllers/auth/support.ts
+++ src/controllers/auth/support.ts
@@ -6,7 +6,8 @@ const cookieOptions: CookieOptions = {
   maxAge: 24 * 60 * 60 * 1000,
   sameSite: "strict",
   secure: true,
-  domain: "https://e-commerce-frontend-0n6s.onrender.com/",
+  partitioned: true,
+  priority: "high",
 };
 
 export { cookieOptions };
